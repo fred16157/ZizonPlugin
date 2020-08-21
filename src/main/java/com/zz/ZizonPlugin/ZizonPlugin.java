5ff654d580a7f54a79b4098e57c2b6a11dc10606
fred16157 <fred16157@gmail.com>
2020-08-21 20:54:59 +0900

    초기 커밋

---
+++ src/main/java/com/zz/ZizonPlugin/ZizonPlugin.java
@@ -0,0 +1,15 @@
+package com.zz.ZizonPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ZizonPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("zl존개쩌는 플러그인이 로드되었습니다!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("zl존개쩌는 플러그인이 종료되었습니다!");
+    }
+}
