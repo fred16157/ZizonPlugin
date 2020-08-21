4fa9e1386a9915bba059cc915490e85a11e5774a
fred16157 <fred16157@gmail.com>
2020-08-21 23:14:29 +0900

    zl존 개쩌는 초고출력 커맨드 추가

---
+++ src/main/java/com/zz/ZizonPlugin/ExplodeCommand.java
@@ -0,0 +1,28 @@
+package com.zz.ZizonPlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+public class ExplodeCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player) {//플래이어
+            Player p = (Player)sender;
+            Bukkit.broadcastMessage(p.getDisplayName() + "님이 zl존 김순상의 초고출력을 시전하셨습니다!");
+            for(int i = 0; i < 10; i++)
+            {
+                p.getWorld().createExplosion(p.getLocation().add(p.getLocation().getDirection().multiply(i)), 10, true, true);
+            }
+        }
+        else if(sender instanceof ConsoleCommandSender) {//콘솔
+            ConsoleCommandSender c = (ConsoleCommandSender)sender;
+            c.sendMessage("콘솔에서는 김순상의 초고출력을 사용할 수 없습니다.");
+        }
+        return true;
+    }
+}
--- src/main/java/com/zz/ZizonPlugin/ZizonPlugin.java
+++ src/main/java/com/zz/ZizonPlugin/ZizonPlugin.java
@@ -8,6 +8,7 @@ public class ZizonPlugin extends JavaPlugin {
         getLogger().info("zl존개쩌는 플러그인이 로드되었습니다!");
         this.getCommand("zizon").setExecutor(new ZizonCommand());
         this.getCommand("lightning").setExecutor(new LightningCommand());
+        this.getCommand("explode").setExecutor(new ExplodeCommand());
     }
 
     @Override
