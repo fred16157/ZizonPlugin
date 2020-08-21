9e524bfc3cb1155209194960a221fab776f9c154
fred16157 <fred16157@gmail.com>
2020-08-21 21:50:27 +0900

    zizon 개쩌는 명령어에 메시지 보내기 추가

--- src/main/java/com/zz/ZizonPlugin/ZizonCommand.java
+++ src/main/java/com/zz/ZizonPlugin/ZizonCommand.java
@@ -3,11 +3,20 @@ package com.zz.ZizonPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 
 public class ZizonCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+        if(sender instanceof Player) {
+            Player p = (Player)sender;
+            p.sendMessage("zl존 개쩌는 명령어를 실행하셨습니다");
+        }
+        else if(sender instanceof ConsoleCommandSender) {
+            ConsoleCommandSender c = (ConsoleCommandSender)sender;
+            c.sendMessage("zl존 개쩌는 명령어를 실행하셨습니다");
+        }
         return true;
     }
 }
