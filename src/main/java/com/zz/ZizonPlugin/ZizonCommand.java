19d14e2ad51626bce2943e032de3c1e5c7ee73e8
JBD <jhlee030313@gmail.com>
2020-08-21 21:59:24 +0900

    우린 부자가 될꺼야!

--- src/main/java/com/zz/ZizonPlugin/ZizonCommand.java
+++ src/main/java/com/zz/ZizonPlugin/ZizonCommand.java
@@ -1,21 +1,27 @@
 package com.zz.ZizonPlugin;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class ZizonCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(sender instanceof Player) {
+        ItemStack diamond = new ItemStack(Material.DIAMOND);
+
+        if(sender instanceof Player) {//플래이어
             Player p = (Player)sender;
-            p.sendMessage("zl존 개쩌는 명령어를 실행하셨습니다");
+            p.sendMessage("player가 zl존 개쩌는 명령어를 실행하셨습니다");
+            diamond.setAmount(1000);
+            p.getInventory().addItem(diamond);
         }
-        else if(sender instanceof ConsoleCommandSender) {
+        else if(sender instanceof ConsoleCommandSender) {//콘솔
             ConsoleCommandSender c = (ConsoleCommandSender)sender;
-            c.sendMessage("zl존 개쩌는 명령어를 실행하셨습니다");
+            c.sendMessage("console이 zl존 개쩌는 명령어를 실행하셨습니다");
         }
         return true;
     }
